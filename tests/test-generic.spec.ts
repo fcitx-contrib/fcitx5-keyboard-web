62af87be2d55adf265234dbf14238e82989a8c71
Qijia Liu <liumeo@pku.edu.cn>
2025-02-18 00:14:33 -0500

    change color on press (#4)

--- tests/test-generic.spec.ts
+++ tests/test-generic.spec.ts
@@ -8,11 +8,16 @@ test('Click', async ({ page }) => {
   await init(page)
 
   const q = page.getByText('q')
+  await expect(q).toHaveCSS('background-color', 'rgb(255, 255, 255)')
+
   await q.dispatchEvent('touchstart')
   expect(await getSentEvents(page), 'To support other gestures, no event should be sent on press.').toEqual([])
+  await expect(q).toHaveCSS('background-color', 'rgb(188, 192, 199)')
+
   await q.dispatchEvent('touchend')
   expect(await getSentEvents(page), '').toEqual([{
     type: 'KEY_DOWN',
     data: { key: 'q', code: '' },
   }])
+  await expect(q).toHaveCSS('background-color', 'rgb(255, 255, 255)')
 })
