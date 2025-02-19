3a20304526d628bd92c65de58d78cbbd049cec4e
Qijia Liu <liumeo@pku.edu.cn>
2025-02-18 23:04:39 -0500

    initial layout change by shift

--- src/ux.ts
+++ src/ux.ts
@@ -1,5 +1,14 @@
-import type { NormalKey } from '../src/layout'
+import type { Key, Layout } from '../src/layout'
 import type { VirtualKeyboardClient } from './api'
+import { renderRow } from './key'
+
+let layout_: Layout
+let currentLayer = 'default'
+let layerLocked = false
+
+export function setLayout(layout: Layout) {
+  layout_ = layout
+}
 
 let client_: VirtualKeyboardClient
 
@@ -29,9 +38,41 @@ export function onTouchStart(event: TouchEvent) {
 export function onTouchEnd() {
   const dataKey = pressedContainer?.getAttribute('data-key')
   if (dataKey) {
-    const pressedKey = JSON.parse(dataKey) as NormalKey
-    client_.sendEvent({ type: 'KEY_DOWN', data: { key: pressedKey.key ?? '', code: pressedKey.code ?? '' } })
+    const pressedKey = JSON.parse(dataKey) as Key
+    switch (pressedKey.type) {
+      case 'key':
+        client_.sendEvent({ type: 'KEY_DOWN', data: { key: pressedKey.key ?? '', code: pressedKey.code ?? '' } })
+        if (!layerLocked) {
+          setLayer('default', false)
+        }
+        break
+      case 'shift':
+        if (currentLayer === 'default') {
+          setLayer('shift', false)
+        }
+        else {
+          setLayer('default', false)
+        }
+        break
+    }
   }
   pressedContainer?.classList.remove('fcitx-keyboard-pressed')
   pressedContainer = null
+}
+
+export function setLayer(id: string, locked: boolean) {
+  for (const layer of layout_.layers) {
+    if (layer.id === id) {
+      currentLayer = id
+      layerLocked = locked
+      const keyboard = document.querySelector('.fcitx-keyboard') as HTMLElement
+      keyboard.innerHTML = ''
+      for (const row of layer.rows) {
+        keyboard.appendChild(renderRow(row, {
+          layer: id,
+          locked,
+        }))
+      }
+    }
+  }
 }
--- tests/test-generic.spec.ts
+++ tests/test-generic.spec.ts
@@ -2,7 +2,7 @@ import {
   expect,
   test,
 } from '@playwright/test'
-import { getSentEvents, init } from './util'
+import { getSentEvents, init, tap } from './util'
 
 test('Click', async ({ page }) => {
   await init(page)
@@ -20,4 +20,28 @@ test('Click', async ({ page }) => {
     data: { key: 'q', code: '' },
   }])
   await expect(q).toHaveCSS('background-color', 'rgb(255, 255, 255)')
+})
+
+test('Shift', async ({ page }) => {
+  await init(page)
+
+  const q = page.getByText('q')
+  const shift = page.locator('.fcitx-keyboard-shift')
+  await tap(shift)
+  await expect(q).toHaveText('Q')
+
+  await tap(q)
+  expect(await getSentEvents(page), '').toEqual([{
+    type: 'KEY_DOWN',
+    data: { key: 'Q', code: '' },
+  }])
+
+  await expect(q).toHaveText('q')
+  await tap(q)
+  const sentEvents = await getSentEvents(page)
+  expect(sentEvents).toHaveLength(2)
+  expect(sentEvents[1]).toEqual({
+    type: 'KEY_DOWN',
+    data: { key: 'q', code: '' },
+  })
 })
