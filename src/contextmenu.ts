65734583a7a799b193eee997e3baa1f5dc116e30
Qijia Liu <liumeo@pku.edu.cn>
2025-06-10 00:14:39 -0400

    support multi actions for a status area button

--- src/contextmenu.ts
+++ src/contextmenu.ts
@@ -1,4 +1,4 @@
-import { div, handleClick, hide, press, release, show } from './util'
+import { div, enableScroll, handleClick, hide, press, release, show } from './util'
 
 export function hideContextMenu() {
   hide(document.querySelector('.fcitx-keyboard-contextmenu-container')!)
@@ -7,6 +7,7 @@ export function hideContextMenu() {
 export function renderContextmenu() {
   const container = div('fcitx-keyboard-contextmenu-container')
   const contextmenu = div('fcitx-keyboard-contextmenu')
+  enableScroll(contextmenu)
   container.appendChild(contextmenu)
   container.addEventListener('touchstart', (event) => {
     // Don't hide if touching menu instead of outside.
@@ -29,17 +30,27 @@ function renderItem(text: string) {
 
 export function showContextmenu(element: Element, items: {
   text: string
+  separator?: boolean
   callback: () => void
 }[]) {
   const contextmenu = document.querySelector('.fcitx-keyboard-contextmenu') as HTMLElement
   contextmenu.innerHTML = ''
-  for (const item of items) {
-    const element = renderItem(item.text)
-    handleClick(element, () => {
-      item.callback()
-      hideContextMenu()
-    })
-    contextmenu.appendChild(element)
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i]
+    if (item.separator) {
+      if (i === 0 || i === items.length - 1 || items[i - 1].separator) {
+        continue
+      }
+      contextmenu.appendChild(document.createElement('hr'))
+    }
+    else {
+      const element = renderItem(item.text)
+      handleClick(element, () => {
+        item.callback()
+        hideContextMenu()
+      })
+      contextmenu.appendChild(element)
+    }
   }
   show(contextmenu.parentElement!)
   const containerBox = contextmenu.parentElement!.getBoundingClientRect()
@@ -48,11 +59,20 @@ export function showContextmenu(element: Element, items: {
   const containerYBar = (containerBox.top + containerBox.bottom) / 2
   const targetYBar = (targetBox.top + targetBox.bottom) / 2
   let y: number
+  // First step: place it above or below the target element.
   if (targetYBar < containerYBar) {
     y = targetBox.bottom
   }
   else {
     y = targetBox.top - menuBox.height
+  }
+  // Next step: adjust the bottom position if it exceeds the container.
+  if (y + menuBox.height > containerBox.bottom) {
+    y = containerBox.bottom - menuBox.height
+  }
+  // Final step: adjust the top position if it exceeds the container.
+  if (y < containerBox.top) {
+    y = containerBox.top
   }
   const x = Math.min(targetBox.left, containerBox.right - menuBox.width)
   contextmenu.style.left = `${x}px`
--- tests/test-status-area.spec.ts
+++ tests/test-status-area.spec.ts
@@ -1,6 +1,6 @@
 import type { Page } from '@playwright/test'
 import { expect, test } from '@playwright/test'
-import { getKey, getSentEvents, getToolbarButton, init, sendSystemEvent, tapReturn } from './util'
+import { getBox, getKey, getSentEvents, getToolbarButton, init, sendSystemEvent, tapReturn } from './util'
 
 function gotoStatusArea(page: Page) {
   return getToolbarButton(page, 5).tap()
@@ -39,4 +39,57 @@ test('Return', async ({ page }) => {
 
   await tapReturn(page)
   await expect(q).toBeVisible()
+})
+
+test('Multiple actions: align bottom', async ({ page }) => {
+  await init(page)
+
+  await sendSystemEvent(page, {
+    type: 'STATUS_AREA',
+    data: [{ desc: '😁 → 😭', children: Array.from({ length: 5 }).map((_, i) => ({
+      desc: `子项${i}`,
+      icon: '',
+      id: i,
+    })), icon: '', id: -1 }],
+  })
+  await gotoStatusArea(page)
+
+  const button = page.locator('.fcitx-keyboard-status-area-circle')
+  await expect(button).toHaveText('😁')
+
+  await button.tap()
+  const contextmenu = page.locator('.fcitx-keyboard-contextmenu')
+  await expect(contextmenu).toBeVisible()
+  const box = await getBox(contextmenu)
+  const containerBox = await getBox(page.locator('.fcitx-keyboard-container'))
+  expect(box.y + box.height).toBeCloseTo(containerBox.y + containerBox.height, 1)
+})
+
+test('Many actions: align top', async ({ page }) => {
+  await init(page)
+
+  await sendSystemEvent(page, {
+    type: 'STATUS_AREA',
+    data: [{ desc: '有 → 无', children: Array.from({ length: 10 }).map((_, i) => ({
+      desc: `子项${i}`,
+      icon: '',
+      id: i,
+    })), icon: '', id: -1 }],
+  })
+  await gotoStatusArea(page)
+
+  const button = page.locator('.fcitx-keyboard-status-area-circle')
+  await expect(button).toHaveText('有')
+
+  await button.tap()
+  const contextmenu = page.locator('.fcitx-keyboard-contextmenu')
+  await expect(contextmenu).toBeVisible()
+  const box = await getBox(contextmenu)
+  const containerBox = await getBox(page.locator('.fcitx-keyboard-container'))
+  expect(box.y).toBeCloseTo(containerBox.y, 1)
+  const lastItem = contextmenu.locator('.fcitx-keyboard-contextmenu-item').last()
+  await expect(lastItem).not.toBeInViewport()
+
+  await contextmenu.evaluate(element => element.scrollBy(0, element.scrollHeight))
+  await expect(lastItem).toBeInViewport()
 })
